a99cdc140f8dc898cd0d42d4a23f9e6763234b66
ryllerfeitosa <you@ryllerfeitosa@gmail.com>
2026-04-17 11:36:19 -0300

    Aula 09 em andamento

---
+++ Exercicios/Aula09/app.js
@@ -0,0 +1,48 @@
+/********************************************************************************************
+ * Objetivo: Arquivo responsável pela criação da API do projeto de filmes
+ * Data: 17.04.26
+ * Nome: Riller
+ * Versão: 1.0
+ ********************************************************************************************/
+
+//Import das dependências para criar a API
+const express        = require('express')
+const cors           = require('cors')
+
+//Import das controllers do projeto
+const controllerFilme = require('./controller/filme/controller_filme.js')
+
+//Import para trabalhar com body -> Comando para instalação: npm install body-parser --save
+const bodyParser     = require('body-parser')
+//Criando um objeto para manipular dados do body da API em formato JSON
+const bodyParserJson = bodyParser.json() 
+
+//Criar um objeto para manipular o express
+const app = express()
+
+//Permissões a serem aplicadas no CORS da API
+const corsOptions = {
+    //Origem da requisição
+    origin:         ['*'],
+    //Tipo de método
+    methods:        'GET, POST, PUT, DELETE, OPTIONS',
+    //Permissões de cabeçalho
+    allowedHeaders: ['Content-type', 'Autorization']
+}
+
+//Configura as permissões da API através do cors
+app.use(cors(corsOptions))
+
+//EndPoints
+
+app.post('/v1/senai/locadora/filme', bodyParserJson, async function(request, response){
+    //Recebe o conteúdo dentro do body da requisição (Abrindo o envelope e guardando o conteúdo da requisição)
+    let dados = request.body
+    let result = await controllerFilme.inserirNovoFilme(dados)
+    response.status(result.status_code)
+    response.json(result)
+})
+
+app.listen(8080, function(){
+    console.log("API funcionando e aguardando novas requisições...")
+})
